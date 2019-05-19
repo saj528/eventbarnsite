0416e5bdf937abbd55939d75dcbf8357d3220da7
Alex <ajmarksboy@gmail.com>
2019-05-18 21:51:44 -0500

    packages page layout, services page finished

---
+++ faq.php
@@ -0,0 +1,64 @@
+<?php require_once 'inc/exheader.php'; ?>
+<br>
+<div class="jumbotron blogtext blog-content">
+  <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+          <div align="center" class="card" >
+
+            <h2 class="textcolorpurple" align="center"><strong>FAQ</strong></h2><br />
+            
+
+            <h2 class="textcolorpurple">Do you have decorations or props for events?</h2>
+            <p>
+                We have a large selection of decorations and props.
+                We want you to enjoy the props and furniture, but we please ask that everything be put back in its original place after the event.
+            </p>
+
+            <br>
+            <h2 class="textcolorpurple">How late can we stay on the night of our event?</h2>
+            <p>
+                All music must end at or before 11:00 pm. And all guests must be gone by 12:00 a.m.
+            </p>
+            
+            <br>
+            <h2 class="textcolorpurple">Can we serve alchohol?</h2>
+            <p>
+                If alcohol is served or consumed, the renter is responsible for complying with all local and state laws concerning alcohol. 
+            </p>
+
+            <br>
+            <h2 class="textcolorpurple">Are there any limits to how we can decorate?</h2>
+            <p>
+                No nails, screws, staples, or penetrating times are to be used on our walls or floors. No glitter or foil confetti is allowed on site. 
+            </p>
+
+            <br>
+            <h2 class="textcolorpurple">I have a special request, will you accommodate me?</h2>
+            <p>
+                You are welcome to make special request.  Please work with Rhonda Unruh on any special request.  We will try to accommodate your needs as much as possible 
+            </p>
+
+            <br>
+            <h2 class="textcolorpurple">Can I have a firearm at my event?</h2>
+            <p>
+                Absolutely no firearms or any other type of weapon of any kind are permitted on the Premises.
+            </p>
+
+            <br>
+            <h2 class="textcolorpurple">Is my deposit refundable?</h2>
+            <p>
+                The Deposit is non-refundable
+            </p>
+
+            <br>
+            <h2 class="textcolorpurple">When is the rental fee due?</h2>
+            <p>
+                The balance of the rental fee is due within 30 days of the event date.
+            </p>
+              
+          </div>
+          
+      </div>
+  </div>
+</div>  
+<?php require_once 'inc/exfooter.php'; ?>
---
+++ packages.php
@@ -0,0 +1,52 @@
+<?php require_once 'inc/exheader.php'; ?>
+<br>
+<div class="jumbotron blogtext blog-content">
+  <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+          <div align="center" class="card" >
+
+            <h2 class="textcolorpurple" align="center"><strong>Packages</strong></h2><br />
+            <em>For more details &amp; pricing of additional hours, please inquire with your Wisteria Vines
+Event Barn coordinator. Please note that prices are subject to change.
+            </em>
+            <br>
+            <br>
+            <br>
+            <h2 class="textcolorpurple">The Story Book Package</h2>
+            <div align="center">
+                <img class="packagesimages roundedimg" src="images/storybook.jpeg" alt="stage set to get married" class="img-responsive roundedimg">
+            </div>
+            <em>Available Throughout the Entire Year</em>
+            <h3>Amenities</h3>
+            <div align="left">
+            <ul>
+              <li>Exclusive 2-day access to the entire ground at Wisteria Vines Event Barn</li>
+              <li>Reception in the ballroom, which is fully equipped for your electrical needs. Dance floor</li>
+              <li>Bridal table for the couple</li>
+              <li>All needed food and beverage tables</li>
+              <li>Private bridal suite with light refreshments</li>
+              <li>Private groom’s quarters with light refreshments</li>
+              <li>(3) 6’ foot tables for your Guestbook, gifts & favors</li>
+              <li>Spacious parking Lot</li>
+              <li>20 round guest tables</li>
+              <li>130 chairs; perfect for an average wedding of 150 guests to set both the ceremony & reception</li>
+              <li>Linens, floor length, for all included tables in your choice of white or ivory</li>
+            </ul>
+
+            <h3 align="center">Pricing</h3>
+            <ul>
+              <li>Monday - Thursday</li>
+              <li>Friday</li>
+              <li>Saturday & Sunday</li>
+                <ul>
+                  <li>1-day</li>
+                  <li>2-day</li>
+                </ul>
+            </ul>
+            </div>
+          </div>
+          
+      </div>
+  </div>
+</div>  
+<?php require_once 'inc/exfooter.php'; ?>
